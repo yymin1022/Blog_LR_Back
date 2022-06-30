f450f050abacb2751169a9fcfba6cb6080b0e63d
yymin102@ <yymin1022@gmail.com>
2022-07-01 00:55:07 +0900

    Update FirebaseUtil.ts : Add Initialize Function

---
+++ src/FirebaseUtil.ts
@@ -0,0 +1,17 @@
+import {initializeApp} from "firebase/app";
+import {collection, doc, getDoc, getDocs, getFirestore, orderBy, query} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    appId: process.env.FB_APP_ID
+};
+
+let firebaseApp;
+let firebaseDB;
+
+export const initFB = () => {
+    firebaseApp = initializeApp(firebaseConfig);
+    firebaseDB = getFirestore();
+}
