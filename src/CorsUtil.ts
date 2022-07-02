4f8d16098772e043b9f618524691cb5d43804b38
yymin102@ <yymin1022@gmail.com>
2022-07-03 03:21:57 +0900

    Update CorsUtil.ts : Import Code from Legacy Repository

---
+++ src/CorsUtil.ts
@@ -0,0 +1,12 @@
+const setCors = () => {
+    const corsList = [process.env.URL_DEV, process.env.URL_PUB];
+    const corsOptions = {
+        origin: (origin : any, callback : any) => {
+            if(corsList.indexOf(origin) !== -1){
+                callback(null, true);
+            }else{
+                callback(new Error("Not Allowed Origin!"));
+            }
+        },
+    };
+}
