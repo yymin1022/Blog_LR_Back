a68ba38deec83cbfe6f25189de57702cedea1155
yymin102@ <yymin1022@gmail.com>
2022-07-03 02:56:20 +0900

    Update index.ts : Import FirebaseUtil Custom Class

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import http from "http";
 
+import * as FB from "./FirebaseUtil";
+
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
